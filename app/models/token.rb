c95795f5072515feee3549de1cceba2ea12dedbd
Robbie Straw <applefreak07@gmail.com>
2010-11-04 11:52:46 -0500

    Made some changes to ensure better constraint on tokens: tokens_controller: increased readability; uses SQL to determine highest turn_order token: added "prepare_destroy" which creates a "cascade effect" of updating token positions index.html.haml: now uses "each_with_index" and displays the index grid.rb: now uses "each_with_index" and displays index
    Signed-off-by: Robbie Straw <applefreak07@gmail.com>

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,7 +1,9 @@
 class Token < ActiveRecord::Base
     belongs_to :grid
     belongs_to :character
-    
+    before_destroy :prepare_destroy
+    #acts_as_list :scope => 'grid_id = #{grid_id} AND tblrow IS NOT NULL AND tblcol IS NOT NULL'
+
     def find_above
       #Return the token above me
     end
@@ -14,5 +16,9 @@ class Token < ActiveRecord::Base
       temp_turn_order = self.turn_order
       self.turn_order = other_token.turn_order
       other_token.turn_order = temp_turn_order
+    end
+
+    def prepare_destroy
+      Token.update_all("turn_order = (turn_order - 1)", "grid_id = #{self.grid_id} AND turn_order > #{self.turn_order}")
     end
 end
