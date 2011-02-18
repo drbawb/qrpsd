5e6be6376a9676baf9554ca6ff4a3137bb3ab4d0
Robbie <rdwarts@commie-64.(none)>
2011-02-17 22:53:16 -0600

    named_scope to scope, since we are using Rails 3 ;p scope now uses a LEFT JOIN with a WHERE clause, this is supposedly faster than using IN(sub-select), for 30-some skills this won't make a huge difference... but writing JOINs is fun!

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,5 +5,9 @@ class Skill < ActiveRecord::Base
   accepts_nested_attributes_for :player_skills
   attr_accessible :name, :description
 	
-	named_scope :deselected, lambda { |*args| {:conditions => ["id NOT IN (SELECT skill_id FROM player_skills WHERE character_id = ?)", (args.first)]} }
+	## scope :deselected, lambda { |*args| {:conditions => ["id NOT IN (SELECT skill_id FROM player_skills WHERE character_id = ?)", (args.first)]} }
+	## above is bad because it uses a NOT IN(sub-select), outer join should be slightly more performant
+	
+	## Uses select("") to disambiguate column names returned by this scope.
+	scope :deselected, select("skills.*").joins("LEFT OUTER JOIN player_skills ON skills.id=player_skills.skill_id").where("player_skills.character_id IS NULL")
 end
